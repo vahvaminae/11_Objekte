80c7f602f35f4888af9fa31cd113acb8e449b7f2
Peter Schmidt <schmidt.multimedia@gmail.com>
2023-07-21 09:58:54 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,10 @@
+# 11_Objekte
+
+### Inspiration:
+- https://www.katebushencyclopedia.com/hounds-of-love-album
+- https://www.katebushencyclopedia.com/hello-earth
+- https://de.wikipedia.org/wiki/Baikalsee
+
+### Objekte in JS:
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+- https://www.w3schools.com/js/js_objects.asp
---
+++ script_01.js
@@ -0,0 +1,44 @@
+
+/***** Erinnerung: Arrays *******/
+
+// let arr;
+// arr = ["Ich","bin","Max"];
+// output(arr);
+// output(arr[0]);
+// output("------------");
+
+
+// // nested arrays - Prinzip: [[[]]]
+// // nested arrays  - hier: [[],[]]
+// arr =   [
+//                 ["Ich","bin","Max"],
+//                 ["Ich","bin","Moritz"]
+//         ];
+
+        
+// output(arr);
+// output(arr[0]);
+// output(arr[0][0]+ " " + arr[1][2]);
+// output(arr[1][0]);
+
+// // nested loop
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         output("Index i: " + i + " Index j: " + j);
+//         output(arr[i][j]);       
+//     }
+// }
+
+
+/***** Objekte 1 Daten/Funktionen *******/
+
+
+
+
+
+
+
+/** Ausgabe */
+function output(outputData) {
+    console.log(outputData);
+}
